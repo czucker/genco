555455d9bcd462ba302decd75b82de3f32ceefa0
czucker <czucker@p3plcpnl0618.prod.phx3.secureserver.net>
2014-09-17 12:25:54 -0700

    daily dev backup

---
+++ wp-content/plugins/add-to-any/addtoany.services.php
@@ -0,0 +1,457 @@
+<?php
+
+$A2A_SHARE_SAVE_services = array(
+
+	"facebook" => array(
+		"name" => "Facebook",
+		"icon" => "facebook",
+	),
+	"twitter" => array(
+		"name" => "Twitter",
+		"icon" => "twitter",
+	),
+	"google_plus" => array(
+		"name" => "Google+",
+		"icon" => "google_plus",
+	),
+	"pinterest" => array(
+		"name" => "Pinterest",
+		"icon" => "pinterest",
+	),
+	"tumblr" => array(
+		"name" => "Tumblr",
+		"icon" => "tumblr",
+	),
+	"stumbleupon" => array(
+		"name" => "StumbleUpon",
+		"icon" => "stumbleupon",
+	),
+	"reddit" => array(
+		"name" => "Reddit",
+		"icon" => "reddit",
+	),
+	"linkedin" => array(
+		"name" => "LinkedIn",
+		"icon" => "linkedin",
+	),
+	"aim" => array(
+		"name" => "AIM",
+		"icon" => "aim",
+	),
+	"aol_mail" => array(
+		"name" => "AOL Mail",
+		"icon" => "aol",
+	),
+	"allvoices" => array(
+		"name" => "Allvoices",
+		"icon" => "allvoices",
+	),
+	"amazon_wish_list" => array(
+		"name" => "Amazon Wish List",
+		"icon" => "amazon",
+	),
+	"app_net" => array(
+		"name" => "App.net",
+		"icon" => "app_net",
+	),
+	"arto" => array(
+		"name" => "Arto",
+		"icon" => "arto",
+	),
+	"baidu" => array(
+		"name" => "Baidu",
+		"icon" => "baidu",
+	),
+	"balatarin" => array(
+		"name" => "Balatarin",
+		"icon" => "balatarin",
+	),
+	"bebo" => array(
+		"name" => "Bebo",
+		"icon" => "bebo",
+	),
+	"bibsonomy" => array(
+		"name" => "BibSonomy",
+		"icon" => "bibsonomy",
+	),
+	"bitty_browser" => array(
+		"name" => "Bitty Browser",
+		"icon" => "bitty",
+	),
+	"blinklist" => array(
+		"name" => "Blinklist",
+		"icon" => "blinklist",
+	),
+	"blip" => array(
+		"name" => "Blip",
+		"icon" => "blip",
+	),
+	"blogmarks" => array(
+		"name" => "BlogMarks",
+		"icon" => "blogmarks",
+	),
+	"blogger_post" => array(
+		"name" => "Blogger Post",
+		"icon" => "blogger",
+	),
+	"browser" => array(
+		"name" => "Bookmark/Favorites",
+		"icon" => "bookmark",
+		"href" => "javascript:if(document.all){window.external.AddFavorite('A2A_LINKURL','A2A_LINKNAME')}else{var b=a2a_config.localize.BookmarkInstructions || 'Press Ctrl+D to bookmark this page';alert(a2a_config.localize.BookmarkInstructions)}",
+		"href_js_esc" => "TRUE",
+	),
+	"bookmarks_fr" => array(
+		"name" => "Bookmarks.fr",
+		"icon" => "bookmarks_fr",
+	),
+	"box_net" => array(
+		"name" => "Box.net",
+		"icon" => "box",
+	),
+	"buddymarks" => array(
+		"name" => "BuddyMarks",
+		"icon" => "buddymarks",
+	),
+	"buffer" => array(
+		"name" => "Buffer",
+		"icon" => "buffer",
+	),
+	"care2_news" => array(
+		"name" => "Care2 News",
+		"icon" => "care2",
+	),
+	"citeulike" => array(
+		"name" => "CiteULike",
+		"icon" => "citeulike",
+	),
+	"dzone" => array(
+		"name" => "DZone",
+		"icon" => "dzone",
+	),
+	"delicious" => array(
+		"name" => "Delicious",
+		"icon" => "delicious",
+	),
+	"design_float" => array(
+		"name" => "Design Float",
+		"icon" => "designfloat",
+	),
+	"diary_ru" => array(
+		"name" => "Diary.Ru",
+		"icon" => "diary_ru",
+	),
+	"diaspora" => array(
+		"name" => "Diaspora",
+		"icon" => "diaspora",
+	),
+	"digg" => array(
+		"name" => "Digg",
+		"icon" => "digg",
+	),
+	"diigo" => array(
+		"name" => "Diigo",
+		"icon" => "diigo",
+	),
+	"email" => array(
+		"name" => "Email",
+		"icon" => "email",
+	),
+	"evernote" => array(
+		"name" => "Evernote",
+		"icon" => "evernote",
+	),
+	"fark" => array(
+		"name" => "Fark",
+		"icon" => "fark",
+	),
+	"flipboard" => array(
+		"name" => "Flipboard",
+		"icon" => "flipboard",
+	),
+	"folkd" => array(
+		"name" => "Folkd",
+		"icon" => "folkd",
+	),
+	"friendfeed" => array(
+		"name" => "FriendFeed",
+		"icon" => "friendfeed",
+	),
+	"funp" => array(
+		"name" => "FunP",
+		"icon" => "funp",
+	),
+	"google_bookmarks" => array(
+		"name" => "Google Bookmarks",
+		"icon" => "google",
+	),
+	"google_gmail" => array(
+		"name" => "Google Gmail",
+		"icon" => "gmail",
+	),
+	"hacker_news" => array(
+		"name" => "Hacker News",
+		"icon" => "y18",
+	),
+	"hatena" => array(
+		"name" => "Hatena",
+		"icon" => "hatena",
+	),
+	"iwiw" => array(
+		"name" => "IWIW",
+		"icon" => "iwiw",
+	),
+	"instapaper" => array(
+		"name" => "Instapaper",
+		"icon" => "instapaper",
+	),
+	"jamespot" => array(
+		"name" => "Jamespot",
+		"icon" => "jamespot",
+	),
+	"jumptags" => array(
+		"name" => "Jumptags",
+		"icon" => "jumptags",
+	),
+	"khabbr" => array(
+		"name" => "Khabbr",
+		"icon" => "khabbr",
+	),
+	"kindle_it" => array(
+		"name" => "Kindle It",
+		"icon" => "kindle",
+	),
+	"line" => array(
+		"name" => "Line",
+		"icon" => "line",
+	),
+	"linkagogo" => array(
+		"name" => "LinkaGoGo",
+		"icon" => "linkagogo",
+	),
+	"linkatopia" => array(
+		"name" => "Linkatopia",
+		"icon" => "linkatopia",
+	),
+	"livejournal" => array(
+		"name" => "LiveJournal",
+		"icon" => "livejournal",
+	),
+	"mail_ru" => array(
+		"name" => "Mail.Ru",
+		"icon" => "mail_ru",
+	),
+	"mendeley" => array(
+		"name" => "Mendeley",
+		"icon" => "mendeley",
+	),
+	"meneame" => array(
+		"name" => "Meneame",
+		"icon" => "meneame",
+	),
+	"mixi" => array(
+		"name" => "Mixi",
+		"icon" => "mixi",
+	),
+	"myspace" => array(
+		"name" => "MySpace",
+		"icon" => "myspace",
+	),
+	"nujij" => array(
+		"name" => "NUjij",
+		"icon" => "nujij",
+	),
+	"netlog" => array(
+		"name" => "Netlog",
+		"icon" => "netlog",
+	),
+	"netvouz" => array(
+		"name" => "Netvouz",
+		"icon" => "netvouz",
+	),
+	"newstrust" => array(
+		"name" => "NewsTrust",
+		"icon" => "newstrust",
+	),
+	"newsvine" => array(
+		"name" => "NewsVine",
+		"icon" => "newsvine",
+	),
+	"nowpublic" => array(
+		"name" => "NowPublic",
+		"icon" => "nowpublic",
+	),
+	"odnoklassniki" => array(
+		"name" => "Odnoklassniki",
+		"icon" => "odnoklassniki",
+	),
+	"oknotizie" => array(
+		"name" => "Oknotizie",
+		"icon" => "oknotizie",
+	),
+	"orkut" => array(
+		"name" => "Orkut",
+		"icon" => "orkut",
+	),
+	"outlook_com" => array(
+		"name" => "Outlook.com",
+		"icon" => "outlook_com",
+	),
+	"phonefavs" => array(
+		"name" => "PhoneFavs",
+		"icon" => "phonefavs",
+	),
+	"pinboard" => array(
+		"name" => "Pinboard",
+		"icon" => "pinboard",
+	),
+	"plurk" => array(
+		"name" => "Plurk",
+		"icon" => "plurk",
+	),
+	"pocket" => array(
+		"name" => "Pocket",
+		"icon" => "pocket",
+	),
+	"print" => array(
+		"name" => "Print",
+		"icon" => "print",
+		"href" => "javascript:print()",
+	),
+	"printfriendly" => array(
+		"name" => "PrintFriendly",
+		"icon" => "printfriendly",
+	),
+	"protopage_bookmarks" => array(
+		"name" => "Protopage Bookmarks",
+		"icon" => "protopage",
+	),
+	"pusha" => array(
+		"name" => "Pusha",
+		"icon" => "pusha",
+	),
+	"qq" => array(
+		"name" => "QQ",
+		"icon" => "qq",
+	),
+	"rediff" => array(
+		"name" => "Rediff MyPage",
+		"icon" => "rediff",
+	),
+	"segnalo" => array(
+		"name" => "Segnalo",
+		"icon" => "segnalo",
+	),
+	"sina_weibo" => array(
+		"name" => "Sina Weibo",
+		"icon" => "sina_weibo",
+	),
+	"sitejot" => array(
+		"name" => "SiteJot",
+		"icon" => "sitejot",
+	),
+	"slashdot" => array(
+		"name" => "Slashdot",
+		"icon" => "slashdot",
+	),
+	"springpad" => array(
+		"name" => "Springpad",
+		"icon" => "springpad",
+	),
+	"startaid" => array(
+		"name" => "StartAid",
+		"icon" => "startaid",
+	),
+	"stumpedia" => array(
+		"name" => "Stumpedia",
+		"icon" => "stumpedia",
+	),
+	"svejo" => array(
+		"name" => "Svejo",
+		"icon" => "svejo",
+	),
+	"symbaloo_feeds" => array(
+		"name" => "Symbaloo Feeds",
+		"icon" => "symbaloo",
+	),
+	"technotizie" => array(
+		"name" => "Technotizie",
+		"icon" => "technotizie",
+	),
+	"tuenti" => array(
+		"name" => "Tuenti",
+		"icon" => "tuenti",
+	),
+	"twiddla" => array(
+		"name" => "Twiddla",
+		"icon" => "twiddla",
+	),
+	"typepad_post" => array(
+		"name" => "TypePad Post",
+		"icon" => "typepad",
+	),
+	"vk" => array(
+		"name" => "VK",
+		"icon" => "vk",
+	),
+	"viadeo" => array(
+		"name" => "Viadeo",
+		"icon" => "viadeo",
+	),
+	"wanelo" => array(
+		"name" => "Wanelo",
+		"icon" => "wanelo",
+	),
+	"webnews" => array(
+		"name" => "Webnews",
+		"icon" => "webnews",
+	),
+	"whatsapp" => array(
+		"name" => "WhatsApp",
+		"icon" => "whatsapp",
+	),
+	"wists" => array(
+		"name" => "Wists",
+		"icon" => "wists",
+	),
+	"wordpress" => array(
+		"name" => "WordPress",
+		"icon" => "wordpress",
+	),
+	"wykop" => array(
+		"name" => "Wykop",
+		"icon" => "wykop",
+	),
+	"xing" => array(
+		"name" => "XING",
+		"icon" => "xing",
+	),
+	"xerpi" => array(
+		"name" => "Xerpi",
+		"icon" => "xerpi",
+	),
+	"yahoo_bookmarks" => array(
+		"name" => "Yahoo Bookmarks",
+		"icon" => "yahoo",
+	),
+	"yahoo_mail" => array(
+		"name" => "Yahoo Mail",
+		"icon" => "yahoo",
+	),
+	"yahoo_messenger" => array(
+		"name" => "Yahoo Messenger",
+		"icon" => "yim",
+	),
+	"yoolink" => array(
+		"name" => "Yoolink",
+		"icon" => "yoolink",
+	),
+	"youmob" => array(
+		"name" => "YouMob",
+		"icon" => "youmob",
+	),
+	"dihitt" => array(
+		"name" => "diHITT",
+		"icon" => "dihitt",
+	),
+
+);
